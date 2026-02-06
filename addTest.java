dbe2334bda6682d810ee98ed140c59bd460140d2
AryaHalmare22 <aryaghalmare22@gmail.com>
2026-02-06 11:45:25 +0530

    Modified addTest file

--- addTest.java
+++ addTest.java
@@ -12,8 +12,11 @@ public class addTest {
 
         int result = Add.add(10, 20);
         int result2 = Add.add(20, 20);
+        int result3 = Add.add(30 , 20);
         assertEquals(30, result);
         assertEquals(40, result2);
+        assertEquals(50 , result2);
+        assertEquals(50 , result3);
     }
 
 
